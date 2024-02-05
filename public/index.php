37ab0a62b64e51c3154a18b23534012e98e6b61b
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-05 18:36:14 -0500

    feat: introduce autoload file for centralized class loading
    Added a new PHP file, autoload.php, to the 'app' directory. This file consolidates the inclusion of necessary configurations, such as config.php and router.php, for centralized class loading in the php_router project. This enhances code organization and ensures a more systematic approach to managing dependencies.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
-    require_once(__DIR__ . '/../app/config.php');
-    require_once(__DIR__ .'/../app/router.php');
+    require_once(__DIR__ . '/../app/autoload.php');
 
     $router = new Router();
     $router->run();
