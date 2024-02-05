ae024889bf9c4c30b869482cdc7fab371072c7a6
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-05 17:59:52 -0500

    feat(controller): add PageController for handling page-related actions
    Introduce a new PHP class, PageController, responsible for handling various page-related actions in the php_router project. The controller includes methods for the 'home,' 'list,' 'modify,' and 'new' functionalities, each echoing a corresponding message.

---
+++ controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+    class PageController {
+        
+        public function home(){
+            echo 'page home';
+        }
+
+        public function list(){
+            echo 'page list';
+        }
+
+        public function modify(){
+            echo 'page modify';
+        }
+
+        public function new(){
+            echo 'page new';
+        }
+    }
