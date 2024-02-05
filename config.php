ef9729bcb8e25cd7572ff3f02a6e8960f22bca4d
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-05 17:57:54 -0500

    feat(config): add URL configuration in config.php
    Added a new configuration file, config.php, to handle URL routing in the php_router project. The file extracts the URL path, considering the base folder, and defines it as the constant 'URL' for use in the application.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    $folder_path = dirname($_SERVER['SCRIPT_NAME']);
+    $url_path = $_SERVER['REQUEST_URI'];
+    $url = substr($url_path, strlen($folder_path));
+
+    define('URL', $url);
