f99adb03847c40605b9dd09f896d1431c0ed70f9
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-05 22:45:17 -0500

    feat(core): enhance render method in Controller class with layout support
    The method now accepts an optional parameter $layout, allowing developers to specify the layout template to be used when rendering the view. Implemented output buffering to capture the rendered view content, which is then injected into the specified layout template.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,6 +1,9 @@
 <?php
     class Controller {
-        protected function render($view){
+        protected function render($view, $layout = 'site'){
+            ob_start();
             require_once(__DIR__ . '/../views/' . $view . 'View.php');
+            $content = ob_get_clean();
+            require_once(__DIR__ . '/../views/layouts/' . $layout . 'Layout.php');
         }
     }
