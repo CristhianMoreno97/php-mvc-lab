1f86a710380f0b6518d8a878a7cc51672f5fbcde
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-05 22:41:43 -0500

    feat(views): introduce site layout view template
    Added a new PHP file, siteLayout.php, in the 'app/views/layouts' directory. This file serves as a layout template for the site's pages, incorporating common HTML structure, styles, and scripts. The dynamic content placeholder <?php echo $content; ?> is included to allow rendering of specific page content within the layout.

---
+++ app/views/layouts/siteLayout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URL_ROOT ?>/assets/css/styles.css">
+    <script src="<?php echo URL_ROOT ?>/assets/js/scripts.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $content; ?>    
+</body>
+</html>
