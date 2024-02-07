474d2556c15a7c5ee567c9f1b6917854d1027bc6
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-07 17:34:25 -0500

    feat(orm): add Orm core class
    Added a new core class, Orm, responsible for providing basic CRUD operations and pagination functionality for database entities.
    
    Changes:
    - Added Orm.php to app/core folder
    - Implemented constructor to initialize properties and establish database connection
    - Included methods for getAll, getById, deleteById, updateById, insert, and paginate operations

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,6 @@
 <?php
     require_once(__DIR__ . '/core/Controller.php');
+    require_once(__DIR__ . '/core/Orm.php');
     require_once(__DIR__ . '/services/Database.php');
     require_once(__DIR__ . '/config.php');
     require_once(__DIR__ .'/router.php');
---
+++ app/core/Orm.php
@@ -0,0 +1,98 @@
+<?php
+
+    class Orm {
+        protected $db;
+        protected $id;
+        protected $table;
+
+        public function __construct($id, $table, PDO $connection) {
+            $this->id = $id;
+            $this->table = $table;
+            $this->db = $connection;
+        }
+
+        public function getAll() {
+            $stmt = $this->db->prepare("SELECT * FROM {$this->table}");
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }
+
+        public function getById($id) {
+            $stmt = $this->db->prepare("SELECT * FROM {$this->table} WHERE id = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch();
+        }
+
+        public function deleteById() {
+            $stmt = $this->db->prepare("DELETE FROM {$this->table} WHERE id = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+        }
+
+        public function updateById($id, $data) {
+            $query = "UPDATE {$this->table} SET ";
+            
+            foreach ($data as $key => $value) {
+                $query .= "{$key} = :{$value}, ";
+            }
+            
+            $query = trim($query, ",");
+            $query .= " WHERE id = :id";
+            
+            $stmt = $this->db->prepare($query);
+            
+            foreach ($data as $key => $value) {
+                $stmt->bindValue(":{$key}", $value);
+            }
+
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+        }
+
+        public function insert($data) {
+            $query = "INSERT INTO {$this->table} (";
+            
+            foreach ($data as $key => $value) {
+                $query .= "{$key},";
+            }
+
+            $query = trim($query, ",") . ") VALUES ( ";
+
+            foreach ($data as $key => $value) {
+                $query .= ":{$value},";
+            }
+
+            $query = trim($query, ",") . ")";
+
+            $stmt = $this->db->prepare($query);
+
+            foreach ($data as $key => $value) {
+                $stmt->bindValue(":{$key}", $value);
+            }
+
+            $stmt->execute();
+        }
+
+        public function paginate($page, $limit) {
+            $offset = ($page -1) * $limit;
+
+            $rows_query = "SELECT COUNT(*) FROM {$this->table}";
+            $rows = $this->db->query($rows_query)->fetchColumn();
+
+            $query = "SELECT * FROM {$this->table} LIMIT {$offset},{$limit}";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+
+            $pages = ceil($rows / $limit);
+            $data = $stmt->fetchAll();
+
+            return [
+                'data' => $data,
+                'page' => $page,
+                'limit' => $limit,
+                'pages' => $pages,
+                'rows' => $rows,
+            ];
+        }
+    }
