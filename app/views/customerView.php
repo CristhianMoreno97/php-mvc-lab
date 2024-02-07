d2f172c28306e9e53366b82a89f875d1774d7382
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-07 18:39:09 -0500

    feat(views): add customerView.php file
    Added a new view file, customerView.php, to represent the customer data in the UI. This file includes an HTML table structure to display customer information and a button to add new customers.
    
    Changes:
    - Added customerView.php to app/views folder
    - Implemented HTML structure for displaying customer data and a button to add new customers
    - Included JavaScript file customer.js for fetching and populating customer data dynamically

---
+++ app/views/customerView.php
@@ -0,0 +1,15 @@
+<a href=""><button>New</button></a>
+<table border="1">
+    <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody id="customerTableBody">        
+    </tbody>
+</table>
+
+<script src="<?php echo URL_ROOT ?>/assets/js/customer.js"></script>
