864f094624dd342425d95305174875f940e31fc2
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-07 21:59:36 -0500

    feat(views): add newCustomerView.php file
    Added a new view file, newCustomerView.php, to represent the form for creating a new customer. This file includes HTML form elements for inputting customer information and a submit button.
    
    Changes:
    - Added newCustomerView.php to app/views folder
    - Implemented HTML form structure for creating a new customer

---
+++ app/views/newCustomerView.php
@@ -0,0 +1,17 @@
+<form action="" id="customer-form">
+    <div>
+        <label for="first-name">First name</label>
+        <input type="text" id="first-name">
+    </div> 
+    <div>
+        <label for="last-name">Last name</label>
+        <input type="text" id="last-name">
+    </div> 
+    <div>
+        <label for="address">Address</label>
+        <input type="text" id="address">
+    </div> 
+    <button type="submit">Save</button>
+</form>
+
+<script src="<?php echo URL_ROOT ?>/assets/js/new-customer.js"></script>
