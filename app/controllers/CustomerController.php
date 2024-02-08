f8e78ad2aa451b478aed853bd9b69d27eb6647ab
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-07 21:42:14 -0500

    feat(customer): update CustomerController methods
    Updated CustomerController.php to include new and create methods for handling customer creation. Also, improved error handling for invalid data and exceptions.
    
    Changes:
    - Added new and create methods to handle customer creation
    - Improved error handling for invalid data and exceptions in the create method

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -13,6 +13,39 @@
             $this->render('customer');
         }
 
+        public function new() {
+            $this->render('newCustomer');
+        }
+
+        public function create() {
+            $res = new Result();
+            $data = file_get_contents('php://input');
+            $body = json_decode($data, true);
+
+            if (!isset($body['firstName'], $body['lastName'], $body['address'])) {
+                $res->success = false;
+                $res->message = 'Invalid data';
+                echo json_encode($res);
+                return;
+            }
+
+            try {
+                $this->customerModel->insert([
+                    'first_name' => $body['firstName'],
+                    'last_name' => $body['lastName'],
+                    'address' => $body['address'],
+                ]);
+
+                $res->success = true;
+                $res->message = 'Customer created';
+            } catch (Exception $e) {
+                $res->success = false;
+                $res->message = $e->getMessage();
+            }
+
+            echo json_encode($res);
+        }
+
         public function table() {
             $res = new Result();
             $customers = $this->customerModel->getAll();
