d10b4ac8b8482d2701cb5a820cc25cad4deb6087
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-08 21:08:01 -0500

    refactor(js): update listCustomer function and add deleteCustomer function in customer.js
    Refactored the listCustomer function in customer.js to properly update the customer table body content by clearing it before appending new rows. Also, added a new deleteCustomer function to handle customer deletion functionality on the client-side.
    
    Changes:
    - Modified listCustomer function in app/public/assets/js/customer.js to clear customer table body before appending new rows
    - Added deleteCustomer function in app/public/assets/js/customer.js to handle customer deletion functionality on the client-side

--- public/assets/js/customer.js
+++ public/assets/js/customer.js
@@ -8,8 +8,9 @@ async function listCustomer() {
 
     if (responseData.success) {
         const customerTableBody = document.getElementById('customerTableBody')
+        customerTableBody.innerHTML = '';
         responseData.result.forEach(item => {
-            customerTableBody.insertAdjacentHTML('beforebegin', `<tr>
+            customerTableBody.insertAdjacentHTML('beforeend', `<tr>
             <td>${item.first_name}</td>
             <td>${item.last_name}</td>
             <td>${item.address}</td>
@@ -17,7 +18,7 @@ async function listCustomer() {
                 <a href="http://localhost/php_router/public/customer/edit/?id=${item.id}">
                     <button>Editar</button>
                 </a>
-                <button>Eliminar</button>
+                <button onclick="deleteCustomer(${item.id})">Eliminar</button>
             </td>
             </tr>`)
         })
@@ -25,3 +26,17 @@ async function listCustomer() {
 }
 
 listCustomer()
+
+async function deleteCustomer(id) {
+    let response = await fetch("http://localhost/php_router/public/customer/delete", {
+        method: 'DELETE',
+        body: JSON.stringify({ id }),
+    })
+    let responseData = await response.json()
+    
+    if (responseData.success) {
+        listCustomer()
+    } else {
+        alert(responseData.message)
+    }
+}
