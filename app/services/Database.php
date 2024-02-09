cc00ce198644a04a6ca77d370210c5746aeb088d
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-08 23:00:45 -0500

    fix(database): update database connection host in Database class
    Updated the host value in the Database class constructor from 'localhost' to '127.0.0.1' to resolve a PDOException issue with PHPUnit tests. This change ensures proper database connection within PHPUnit environment.
    
    Changes:
    - Modified host value in Database class constructor in app/services/database.php to '127.0.0.1' for PHPUnit compatibility

--- app/services/Database.php
+++ app/services/Database.php
@@ -9,7 +9,7 @@
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ];
 
-            $this->connection = new PDO('mysql:host=localhost;dbname=ecommerce', 'root', '', $options);
+            $this->connection = new PDO('mysql:host=127.0.0.1;dbname=ecommerce', 'root', '', $options);
             $this->connection->exec("SET CHARACTER SET UTF8");
         }
 
